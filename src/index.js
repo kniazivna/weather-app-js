b75e9c07b078c7ae79b76db7122e0bf7205e20c9
kniazivna <91503586+kniazivna@users.noreply.github.com>
2022-09-13 13:45:36 +0300

    add icons from api

--- src/index.js
+++ src/index.js
@@ -1,46 +1,63 @@
-function showDate(timestamp){
+function showDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thuersday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
 
-    let date = new Date(timestamp);
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thuersday", "Friday", "Saturday"]
-    let day = days[date.getDay()];
-    let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-    if(hours < 10){
-        hours = `0${hours}`
-    }
+  let minutes = date.getMinutes();
 
-    let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-
-    return `${day} ${hours}:${minutes}`
+  return `${day} ${hours}:${minutes}`;
 }
 
-function showTemperature(response){
-console.log(response.data);
+function showTemperature(response) {
+  console.log(response.data);
 
-let temperature = document.querySelector("#temperature");
-temperature.innerHTML = Math.round(response.data.main.temp); 
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
 
-let city = document.querySelector("#city");
-city.innerHTML = response.data.name;
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
 
-let description = document.querySelector("#description");
-description.innerHTML = response.data.weather[0].description;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
 
-let humidity = document.querySelector("#humidity");
-humidity.innerHTML = response.data.main.humidity;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
 
-let wind = document.querySelector("#wind");
-wind.innerHTML = Math.round(response.data.wind.speed);
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
 
-let date = document.querySelector("#date");
-date.innerHTML = showDate(response.data.dt * 1000);
+  let date = document.querySelector("#date");
+  date.innerHTML = showDate(response.data.dt * 1000);
+
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt",
+   response.data.weather[0].description);
 }
+
 let apiKey = "1a113ff0323f06b582adfa8290155b6a";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`;
+let searchCity = "Lviv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
