0e2ca87f939f596d6d10d42a254784e36ed32ace
kniazivna <91503586+kniazivna@users.noreply.github.com>
2022-09-13 13:21:05 +0300

    add api call

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function showDate(timestamp){
+
+    let date = new Date(timestamp);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thuersday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+
+    if(hours < 10){
+        hours = `0${hours}`
+    }
+
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
+}
+
+function showTemperature(response){
+console.log(response.data);
+
+let temperature = document.querySelector("#temperature");
+temperature.innerHTML = Math.round(response.data.main.temp); 
+
+let city = document.querySelector("#city");
+city.innerHTML = response.data.name;
+
+let description = document.querySelector("#description");
+description.innerHTML = response.data.weather[0].description;
+
+let humidity = document.querySelector("#humidity");
+humidity.innerHTML = response.data.main.humidity;
+
+let wind = document.querySelector("#wind");
+wind.innerHTML = Math.round(response.data.wind.speed);
+
+let date = document.querySelector("#date");
+date.innerHTML = showDate(response.data.dt * 1000);
+}
+let apiKey = "1a113ff0323f06b582adfa8290155b6a";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(showTemperature);
